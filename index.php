cc5018fe8d7997136c28784497bd9916d090214f
not-ted <ramos.nomar27@gmail.com>
2023-10-03 22:18:28 -0500

    renamed login.php/css/js to index.php/css/js

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+<head>
+	<link rel = "stylesheet" href = "index.css">
+	<meta charset = "UTF-8">
+	<meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+	<title>Login</title>
+</head>
+
+<h1>University Library</h1>
+<body>
+	<div></h2>Login</h2></div>
+	<form action = 'connenct.php' method = "POST">
+		<div class = "input">
+			<label for = "username">Username</label>
+			<input type = "text" name = "username" id = "username" required>
+		</div>
+		<div class = "input">
+			<label for = "password">Password</label>
+			<input type = "password" name = "password" id = "password" required>
+		</div>
+		<div class = "input">
+			<input type = "submit" value = "Login">
+		</div>
+</body>
+</html>
