9951ba1931874a5a97558c76cadf8687c03e9f11
not-ted <ramos.nomar27@gmail.com>
2023-10-09 18:46:01 -0500

    hello

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!--- This is the login page for the library management system. --->
+<?php require 'connection.php'; ?>
+<?php
+session_start();
+ 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // TODO: Check if the username and password are valid
+
+    if ($username == 'admin' && $password == 'password') {
+        $_SESSION['loggedin'] = true;
+        header('Location: index.php');
+        exit;
+    } 
+	else {
+        $error = 'Invalid username or password';
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang = "en">
 
@@ -10,8 +32,8 @@
 
 <h1>University Library</h1>
 <body>
-	<div></h2>Login</h2></div>
-	<form action = 'connenct.php' method = "POST">
+	<div><h2>Login</h2></div>
+	<form action = 'connection.php' method = "POST">
 		<div class = "input">
 			<label for = "username">Username</label>
 			<input type = "text" name = "username" id = "username" required>
