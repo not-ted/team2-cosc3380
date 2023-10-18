e1994c85c4bb312481e04b0e885cd85321338540
Jinn Ha <124510393+hpt155722@users.noreply.github.com>
2023-10-18 18:13:53 -0500

    Update itemAdd.php

---
+++ pages/item add/itemAdd.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Item Add</title>
+
+    <!-- Import External Stylesheet -->
+    <link rel="stylesheet" href="itemAdd.css">
+
+    <!-- Import jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+</head>
+<body onload = 'itemAddOnload()'>
+  <div class = 'verContainer'>
+    <div class = 'horContainer'>
+        <div class = 'itemAddEntireContainer'> 
+
+        </div>
+    </div>
+  </div>
+</body>
+</html>
