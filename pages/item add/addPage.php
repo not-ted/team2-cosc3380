6fb63ea7d2d2b9186e8a0b395393ff6865ba2e7a
Jinn Ha <124510393+hpt155722@users.noreply.github.com>
2023-10-18 18:39:25 -0500

    Update addPage.php

--- pages/item add/addPage.php
+++ pages/item add/addPage.php
@@ -1,1 +1,37 @@
+<?php
+    include ("../../connection.php");
 
+    $itemType = $_GET['itemType'];
+    if ($itemType == "book")
+    {
+        $bookName = $_GET['bookName'];
+        $ISBN = $_GET['ISBN'];
+        $author = $_GET['author'];     
+        $publicationCompany = $_GET['publicationCompany'];     
+        $publishedDate = $_GET['publishedDate'];     
+        $paperbackCopiesAvailiable = $_GET['paperbackCopiesAvailiable'];      
+        $paperbackCopyValue = $_GET['paperbackCopyValue'];      
+        $hardbackCopiesAvailiable = $_GET['hardbackCopiesAvailiable'];     
+        $hardbackCopyValue = $_GET['hardbackCopyValue'];      
+    }
+    else if ($itemType == "movie")
+    {
+        $movieName = $_GET['movieName'];
+        $distributedBy = $_GET['distributedBy'];
+        $director = $_GET['director'];     
+        $publicationCompany = $_GET['publicationCompany'];     
+        $publishedDate = $_GET['publishedDate'];     
+        $copiesAvailiable = $_GET['copiesAvailiable'];      
+        $copyValue = $_GET['copyValue'];      
+    }
+    else //If itemType is tech
+    {
+        $techName = $_GET['techName'];
+        $modelNumber = $_GET['modelNumber'];
+        $publishedDate = $_GET['publishedDate'];     
+        $brandName = $_GET['brandName'];     
+        $serialNumber = $_GET['serialNumber'];     
+        $copiesAvailiable = $_GET['copiesAvailiable'];      
+        $copyValue = $_GET['copyValue'];      
+    }
+?>
