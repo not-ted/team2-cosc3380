3a5e394315bbe6166951f43f3232aa8906202704
Widyan Hussien <widyanhussien@gmail.com>
2023-10-26 03:27:59 -0500

    hi

--- pages/user search/userSearch.php
+++ pages/user search/userSearch.php
@@ -1,44 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Library User Search</title>
+    <title>User Search</title>
     <link rel="stylesheet" type="text/css" href="userSearch.css">
 </head>
 <body>
-    <h1>Library User Search</h1>
+    <h1>User Search</h1>
     <div class="search-container">
         <form method="post" action="">
-            <input type="text" name="search" placeholder="Search..." value="<?php echo isset($_POST['search']) ? $_POST['search'] : ''; ?>">
             <select name="userType">
-                <option value="all" <?php echo ($_POST['userType'] === 'all') ? 'selected' : ''; ?>>All Users</option>
-                <option value="faculty" <?php echo ($_POST['userType'] === 'faculty') ? 'selected' : ''; ?>>Faculty</option>
-                <option value="students" <?php echo ($_POST['userType'] === 'students') ? 'selected' : ''; ?>>Students</option>
+                <option value="all">All Users</option>
+                <option value="faculty">Faculty</option>
+                <option value="students">Students</option>
             </select>
-            <button type="submit">Search</button>
+            <button type="submit" name="search-button">Search</button>
         </form>
     </div>
     <div id="results">
         <?php
-        $host = "localhost";
-        $database = "librarydatabase";
-        $username = "root"; // Replace with your database username
-        $password = ""; // Replace with your database password
+        if (isset($_POST['search-button'])) {
+            $host = "localhost";
+            $database = "librarydatabase";
+            $username = "root"; // Replace with your database username
+            $password = ""; // Replace with your database password (if any)
 
-        $mysqli = new mysqli($host, $username, $password, $database);
+            $mysqli = new mysqli($host, $username, $password, $database);
 
-        if ($mysqli->connect_error) {
-            die("Connection failed: " . $mysqli->connect_error);
-        }
+            if ($mysqli->connect_error) {
+                die("Connection failed: " . $mysqli->connect_error);
+            }
 
-        if (isset($_POST['search'])) {
-            $search = $_POST['search'];
-            $userType = $_POST['userType'];
+            $userType = $_POST["userType"];
 
             // Prepare the SQL statement based on the selected user type
-            if ($userType === 'all') {
-                $sql = "SELECT * FROM users WHERE firstName LIKE '%$search'";
+            if ($userType === "all") {
+                $sql = "SELECT * FROM users";
             } else {
-                $sql = "SELECT * FROM users WHERE userType = '$userType' AND firstName LIKE '%$search'";
+                $sql = "SELECT * FROM users WHERE userType = '$userType'";
             }
 
             $result = $mysqli->query($sql);
@@ -46,21 +44,29 @@
             // Display the search results in a table
             echo '<table>';
             echo '<tr>';
-            echo '<th>Username</th>';
+            echo '<th>User ID</th>';
+            echo '<th>UH ID</th>';
             echo '<th>User Type</th>';
+            echo '<th>Email</th>';
+            echo '<th>First Name</th>';
+            echo '<th>Last Name</th>';
             echo '</tr>';
 
             while ($row = $result->fetch_assoc()) {
                 echo '<tr>';
-                echo '<td>' . $row['firstName'] . '</td>';
+                echo '<td>' . $row['userID'] . '</td>';
+                echo '<td>' . $row['uhID'] . '</td>';
                 echo '<td>' . $row['userType'] . '</td>';
+                echo '<td>' . $row['email'] . '</td>';
+                echo '<td>' . $row['firstName'] . '</td>';
+                echo '<td>' . $row['lastName'] . '</td>';
                 echo '</tr>';
             }
             echo '</table>';
-        }
 
-        // Close the database connection
-        $mysqli->close();
+            // Close the database connection
+            $mysqli->close();
+        }
         ?>
     </div>
     <script src="userSearch.js"></script>
