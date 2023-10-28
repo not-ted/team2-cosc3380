4937ecee94056e9f47a44119f29388a2e7ec0676
not-ted <ramos.nomar27@gmail.com>
2023-10-28 02:23:50 -0500

    user details

---
+++ pages/user-detail/detailFunctions.php
@@ -0,0 +1,191 @@
+<?php
+include("../../connection.php");
+
+function changeBorrow($userID, $conn){
+	$query = "UPDATE users SET canBorrow = !canBorrow WHERE userID = '$userID' LIMIT 1";
+	$conn->query($query);
+	echo "<script>alert('Borrow privilege changed!');</script>";
+}
+
+function getFines($userID, $conn){
+	$query = "SELECT * FROM fines WHERE userID = '$userID'";
+	$result = mysqli_query($conn, $query);
+	if (mysqli_num_rows($result) > 0) {
+		while($row = mysqli_fetch_assoc($result)){
+			echo "<tr>";
+			echo "<td>" . $row["fineAmount"] . "</td>";
+			echo "<td>" . $row["type"] . "</td>";
+			if($row['havePaid'] == 1){
+				echo "<td style='color:green'>PAID</td>";
+			}
+			else{
+				echo "<td style='color:red'>UNPAID</td>";
+			}
+		}
+	}
+}
+
+function isLate($row){
+	if($row["dueDate"] < date("Y-m-d")){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+function isAvailable($row, $conn){
+	$itemID = $row['itemID'];
+	if($row['itemType'] == "book"){
+		$query = "SELECT * FROM bookCopy WHERE bookID = '$itemID' && available = 1";
+		$result = mysqli_query($conn, $query);
+	}
+	else if($row['itemType'] == "movie"){
+		$query = "SELECT * FROM movieCopy WHERE movieID = '$itemID' && available = 1";
+		$result = mysqli_query($conn, $query);
+	}
+	else if($row['itemType'] == "tech"){
+		$query = "SELECT * FROM techCopy WHERE techID = '$itemID' && available = 1";
+		$result = mysqli_query($conn, $query);
+	}
+	if (mysqli_num_rows($result) > 0) {
+		return true;
+	} 
+	else {
+		return false;
+	}
+}	
+
+function displayCheckouts($result, $conn, $itemType){
+	if (mysqli_num_rows($result) > 0) {
+		while($row = mysqli_fetch_assoc($result)){
+			echo "<tr>";
+			echo "<td>" . $row["itemName"] . "</td>";
+			echo "<td>" . $itemType . "</td>";
+			echo "<td>" . $row["checkoutDate"] . "</td>";
+			if($row['returnedDate'] == NULL){
+				echo "<td>" . $row["dueDate"] . "</td>";
+			}
+			else{
+				echo "<td>" . $row["returnedDate"] . "</td>";
+			}
+			if(isLate($row)){
+				echo "<td style='color:red'>OVERDUE</td>";
+			}
+			else{
+				echo "<td>--</td>";
+			}
+		}
+	}
+}
+
+function displayHolds($result, $conn, $itemType){
+	if (mysqli_num_rows($result) > 0) {
+		while($row = mysqli_fetch_assoc($result)){
+			echo "<tr>";
+			echo "<td>" . $row["itemName"] . "</td>";
+			echo "<td>" . $itemType . "</td>";
+			echo "<td>" . $row["requestDate"] . "</td>";
+			if(isAvailable($row, $conn)){
+				echo "<td style='color:green'>AVAILABLE</td>";
+			}
+			else{
+				echo "<td style='color:red'>UNAVAILABLE</td>";
+			}
+		}
+	}
+}
+
+function getCheckouts($userID, $conn){
+	// create prepared statements
+	$bookQ = 	"SELECT B.bookName AS itemName, D.checkoutDate, D.dueDate 
+    				FROM borrowed AS D
+    				JOIN bookCopy AS C ON D.itemCopyID = C.bookCopyID
+    				JOIN books AS B ON C.bookID = B.bookID
+    				WHERE D.userID = '$userID' AND D.itemType = 'book' AND D.returnedDate IS NULL";
+
+	$getBooks = mysqli_query($conn, $bookQ);
+
+	$movieQ = 	"SELECT M.movieName AS itemName, D.checkoutDate, D.dueDate 
+					FROM borrowed AS D
+					JOIN moviecopy AS C ON D.itemCopyID = C.movieCopyID
+					JOIN movies AS M ON C.movieID = M.movieID
+					WHERE D.userID = '$userID' AND D.itemType = 'movie' AND D.returnedDate IS NULL";
+				
+	$getMovies = mysqli_query($conn, $movieQ);
+
+	$techQ = 	"SELECT T.techName AS itemName, D.checkoutDate, D.dueDate 
+					FROM borrowed AS D
+					JOIN techcopy AS C ON D.itemCopyID = C.techCopyID
+					JOIN tech AS T ON C.techID = T.techID
+					WHERE D.userID = '$userID' AND D.itemType = 'tech' AND D.returnedDate IS NULL";
+
+	$getTech = mysqli_query($conn, $techQ);
+
+	displayCheckouts($getBooks, $conn, "book");
+	displayCheckouts($getMovies, $conn, "movie");
+	displayCheckouts($getTech, $conn, "tech");
+
+}
+
+function getHolds($userID, $conn){
+	// create prepared statements
+	$bookQ = 	"SELECT bookName, requestDate, itemType 
+					FROM holds
+					JOIN books ON itemID = bookID
+					WHERE userID = '$userID' AND itemType = 'book' AND requestStatus = 'pending'";
+
+	$getBooks = mysqli_query($conn, $bookQ);
+
+	$movieQ = 	"SELECT movieName, requestDate, itemType 
+					FROM holds
+					JOIN movies ON itemID = movieID
+					WHERE userID = '$userID' AND itemType = 'movie' AND requestStatus = 'pending'";
+
+	$getMovies = mysqli_query($conn, $movieQ);
+
+	$techQ = 	"SELECT techName, requestDate, itemType 
+					FROM holds
+					JOIN tech ON itemID = techID
+					WHERE userID = '$userID' AND itemType = 'tech' AND requestStatus = 'pending'";
+
+	$getTech = mysqli_query($conn, $techQ);
+
+	displayHolds($getBooks, $conn, "book");
+	displayHolds($getMovies, $conn, "movie");
+	displayHolds($getTech, $conn, "tech");
+
+}
+
+function getHistory($userID, $conn){
+	// create prepared statements
+	$bookQ = 	"SELECT B.bookName AS itemName, D.checkoutDate, D.dueDate 
+    				FROM borrowed AS D
+ 					JOIN bookCopy AS C ON D.itemCopyID = C.bookCopyID
+    				JOIN books AS B ON C.bookID = B.bookID
+    				WHERE D.userID = '$userID' AND D.itemType = 'book' AND D.returnedDate IS NOT NULL";
+
+	$getBooks = mysqli_query($conn, $bookQ);
+
+	$movieQ = 	"SELECT M.movieName AS itemName, D.checkoutDate, D.dueDate 
+					FROM borrowed AS D
+					JOIN moviecopy AS C ON D.itemCopyID = C.movieCopyID
+					JOIN movies AS M ON C.movieID = M.movieID
+					WHERE D.userID = '$userID' AND D.itemType = 'movie' AND D.returnedDate IS NOT NULL";
+
+	$getMovies = mysqli_query($conn, $movieQ);
+
+	$techQ =	"SELECT T.techName AS itemName, D.checkoutDate, D.dueDate 
+					FROM borrowed AS D
+					JOIN techcopy AS C ON D.itemCopyID = C.techCopyID
+					JOIN tech AS T ON C.techID = T.techID
+					WHERE D.userID = '$userID' AND D.itemType = 'tech' AND D.returnedDate IS NOT NULL";
+
+	$getTech = mysqli_query($conn, $techQ);
+
+	displayCheckouts($getBooks, $conn, "book");
+	displayCheckouts($getMovies, $conn, "movie");
+	displayCheckouts($getTech, $conn, "tech");
+
+}
+?>
---
+++ pages/user-detail/userDetail.php
@@ -0,0 +1,130 @@
+<?php
+session_start();
+include("detailFunctions.php");
+
+if(!isset($_SESSION['user_id'])){
+	header("Location: ../../index.php");
+}
+
+// check if a user ID has been passed in the URL
+//if (isset($_GET["id"])) {
+  // get the user ID from the URL
+  //$userId = $_GET["id"];
+  $userId = 2;
+
+  // query the database for the user with the specified ID
+  $query = "SELECT * FROM users WHERE userID = $userId LIMIT 1";
+  $result = mysqli_query($conn, $query);
+
+  if (mysqli_num_rows($result) > 0) {
+    $user_info = mysqli_fetch_assoc($result);
+  } 
+  else {
+    echo "User not found";
+  }
+//}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="userDetail.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>User Detail</title>
+</head>
+
+<body>
+
+	<div class="logout-container">
+		<button class="back-button" onclick="location.href='../../userSearch.php'">Back to Search</button>
+        <button class="logout-button" onclick="location.href='../../logout.php'">Logout</button>
+    </div>
+
+	<h1><?php echo htmlspecialchars($user_info['firstName']) . " " . htmlspecialchars($user_info['lastName']); ?></h1>
+    <ul class="user-profile">
+        <li class="user-profile-item">User ID: <?php echo htmlspecialchars($user_info['uhID']); ?></li>
+        <li class="user-profile-item">Email: <?php echo htmlspecialchars($user_info['email']); ?></li>
+        <li class="user-profile-item">User Type: <?php echo htmlspecialchars($user_info['userType']); ?></li>
+        <li class="user-profile-item">Can Borrow: <?php echo ($user_info['canBorrow'] == 1) ? 'Yes' : 'No'; ?></li>
+        <li class="user-profile-item">Borrow Limit (days): <?php echo htmlspecialchars($user_info['borrowLimit']); ?></li>
+    </ul>
+
+	<div class="edit-container">
+  		<button class="edit-button" onclick="<?php changeBorrow($userID, $conn)?>">Change Borrow Privilege</button>		
+		<button class="edit-button" onclick="showFines()">Clear Fine</button>	
+	</div>
+
+	<script>
+		function showFines() {
+  			document.getElementById("fines").style.display = "block";
+		}
+	</script>
+
+	<div id="fines" class="fines-container" style="display:none">
+		<h2>Fines</h2>
+		<table class="generic-table">
+			<thead>
+				<tr>
+					<th>Fine Amount</th>
+					<th>Fine Type</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php getFines($userId, $conn); ?>
+			</tbody>
+		</table>
+  	</div>				
+
+	<h2>Currently Borrowed</h2>
+	<table class="generic-table">
+        <thead>
+            <tr>
+                <th>Item Name</th>
+				<th>Item Type</th>
+                <th>Checkout Date</th>
+                <th>Due Date</th>
+				<th>Status</th>
+            </tr>
+        </thead>
+		<tbody>
+			<?php getCheckouts($userId, $conn); ?>
+		</tbody>
+	</table>
+
+	<h2>Current Hold Requests</h2>
+	<table class="generic-table">
+        <thead>
+            <tr>
+                <th>Item Name</th>
+				<th>Item Type</th>
+                <th>Request Date</th>
+				<th>Status</th>
+            </tr>
+        </thead>
+		<tbody>
+			<?php getHolds($userId, $conn); ?>
+		</tbody>
+	</table>
+
+  	<h2>Checkout History</h2>	
+	<table class="generic-table">
+		<thead>
+			<tr>
+				<th>Item Name</th>
+				<th>Item Type</th>
+				<th>Checkout Date</th>
+				<th>Return Date</th>
+				<th>Status</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php getHistory($userId, $conn); ?>
+		</tbody>
+
+</body>
+
+</html>
