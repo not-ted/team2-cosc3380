b4969136082a1118be769b0e5836feeff1045a4b
not-ted <ramos.nomar27@gmail.com>
2023-11-12 12:40:08 -0600

    added new functions to update item information

--- pages/item detail/itemFunctions.php
+++ pages/item detail/itemFunctions.php
@@ -5,28 +5,32 @@ function getAuthors($itemInfo, $conn){
 	$bookID = $itemInfo['bookID'];
 	$query = "SELECT authorName FROM authors AS A, writtenby AS W WHERE bookID = $bookID AND A.authorID = W.authorID";
 	$result = mysqli_query($conn, $query);
+	$authors = array();
 	if (mysqli_num_rows($result) > 0) {
-		while($row = mysqli_fetch_assoc($result)){
-			echo htmlspecialchars($row['authorName']) . " ";
-		}
-	}
-	else{
-		echo "No authors found";
-	}
+        while($row = mysqli_fetch_assoc($result)){
+            $authors[] = htmlspecialchars($row['authorName']);
+        }
+        echo implode(", ", $authors);
+    }
+    else{
+        echo "No authors found";
+    }
 }
 
 function getDirector($itemInfo, $conn){
 	$movieID = $itemInfo['movieID'];
 	$query = "SELECT directorName FROM directors AS D, directedby AS B WHERE movieID = $movieID AND D.directorID = B.directorID";
 	$result = mysqli_query($conn, $query);
+	$directors = array();
 	if (mysqli_num_rows($result) > 0) {
-		while($row = mysqli_fetch_assoc($result)){
-			echo htmlspecialchars($row['directorName']) . " ";
-		}
-	}
-	else{
-		echo "No directors found";
-	}
+        while($row = mysqli_fetch_assoc($result)){
+            $directors[] = htmlspecialchars($row['authorName']);
+        }
+        echo implode(", ", $directors);
+    }
+    else{
+        echo "No authors found";
+    }
 }
 
 function getBrand($itemInfo, $conn){
@@ -52,7 +56,7 @@ function getYear($itemInfo, $conn){
 			echo htmlspecialchars($row['publishedDate']);
 		}
 		else{
-			echo "No year found";
+			echo htmlspecialchars("No year found");
 		}
 }
 
@@ -167,6 +171,191 @@ function getWaitlist($itemInfo, $conn, $itemType){
 		else{
 			echo "<tr><td colspan='3'>Waitlist is empty</td></tr>";
 		}
+	}
+}
+
+function updateBook($itemID, $conn){
+	$bookName = $_POST['title'];
+	$author = $_POST['author'];
+	$description = mysqli_real_escape_string($conn, $_POST['description']);
+	$genre = $_POST['genre'];
+    $ISBN = $_POST['ISBN'];
+    $publicationCompany = $_POST['publisher'];
+    $publishedDate = $_POST['publishDate'];
+
+	$query = "UPDATE books SET bookName = '$bookName', genre = '$genre', ISBN = '$ISBN', publicationCompany = '$publicationCompany' WHERE bookID = '$itemID'";
+	$query2 = "UPDATE item_description SET description = '$description' WHERE itemID = '$itemID' AND itemType = 'book'";
+	$query3 = "UPDATE bookcopy SET publishedDate = '$publishedDate' WHERE bookID = '$itemID'";
+	updateAuthor($itemID, $author, $conn);
+
+	if(mysqli_query($conn, $query) && mysqli_query($conn, $query2) && mysqli_query($conn, $query3)){
+		$_SESSION['message'] = "Item updated successfully";
+	}
+	else{
+		$_SESSION['message'] = "Error updating item";
+	}
+}
+
+function updateMovie($itemID, $conn){
+	$movieName = $_POST['title'];
+	$director = $_POST['director'];
+	$description = $_POST['description'];
+	$productionCompany = $_POST['productionCompany'];
+	$genre = $_POST['genre'];
+	$releaseDate = $_POST['releaseDate'];
+
+	$query = "UPDATE movies SET movieName = '$movieName', productionCompany = '$productionCompany', genre = '$genre', publishedDate = '$releaseDate' WHERE movieID = '$itemID'";
+	$query2 = "UPDATE item_description SET description = '$description' WHERE itemID = '$itemID' AND itemType = 'movie'";
+	updateDirector($itemID, $director, $conn);
+
+	if(mysqli_query($conn, $query) && mysqli_query($conn, $query2)){
+		$_SESSION['message'] = "Item updated successfully";
+	}
+	else{
+		$_SESSION['message'] = "Error updating item";
+	}
+}
+
+function updateTech($itemID, $conn){
+	$techName = $_POST['title'];
+	$brand = $_POST['brand'];
+	$description = $_POST['description'];
+	$model = $_POST['model'];
+
+	$query = "UPDATE tech SET techName = '$techName', modelNumber = '$model' WHERE techID = '$itemID'";
+	$query2 = "UPDATE item_description SET description = '$description' WHERE itemID = '$itemID' AND itemType = 'tech'";
+	updateBrand($itemID, $brand, $conn);
+
+	if(mysqli_query($conn, $query) && mysqli_query($conn, $query2)){
+		$_SESSION['message'] = "Item updated successfully";
+	}
+	else{
+		$_SESSION['message'] = "Error updating item";
+	}
+}
+
+function updateAuthor($itemID, $author, $conn){
+	$authorArray = explode(", ", $author);
+
+	foreach($authorArray as $authorName){
+		//check if author exists in database
+		$query = "SELECT * FROM authors WHERE authorName = '$authorName'";
+		$result = mysqli_query($conn, $query);
+		if (mysqli_num_rows($result) > 0) {
+			//if Author exists check if author is already associated with book
+			$row = mysqli_fetch_assoc($result);
+			$authorID = $row['authorID'];
+			$query2 = "SELECT * FROM writtenBy WHERE authorID = '$authorID' AND bookID = '$itemID'";
+			$result2 = mysqli_query($conn, $query2);
+			if (mysqli_num_rows($result2) > 0) {
+				//if author is already associated with book, do nothing
+			}
+			else{
+				//if author is not associated with book, add to writtenby table
+				$query3 = "INSERT INTO writtenBy (authorID, bookID) VALUES ('$authorID', '$itemID')";
+				mysqli_query($conn, $query3);
+			}
+		}
+		else{
+			//if author does not exist, add to database
+			$query4 = "INSERT INTO authors (authorName) VALUES ('$authorName')";
+			mysqli_query($conn, $query4);
+			//get authorID
+			$authorID = mysqli_insert_id($conn);
+			//add to writtenby table
+			$query6 = "INSERT INTO writtenBy (authorID, bookID) VALUES ('$authorID', '$itemID')";
+			mysqli_query($conn, $query6);
+		}
+	}
+	//delete any authors that are no longer associated with book
+	$query5 = "SELECT * FROM authors JOIN writtenBy ON authors.authorID = writtenBy.authorID WHERE bookID = '$itemID'";
+	$result3 = mysqli_query($conn, $query5);
+	if (mysqli_num_rows($result3) > 0) {
+		while($row = mysqli_fetch_assoc($result3)){
+			if(!in_array($row['authorName'], $authorArray)){
+				$authorID = $row['authorID'];
+				$query7 = "DELETE FROM writtenBy WHERE authorID = '$authorID' AND bookID = '$itemID'";
+				mysqli_query($conn, $query7);
+			}
+		}
+	}
+}
+
+function updateDirector($itemID, $director, $conn){
+	$directorArray = explode(", ", $director);
+
+	foreach($directorArray as $directorName){
+		//check if director exists in database
+		$query = "SELECT * FROM directors WHERE directorName = '$directorName'";
+		$result = mysqli_query($conn, $query);
+		if (mysqli_num_rows($result) > 0) {
+			//if director exists check if director is already associated with movie
+			$row = mysqli_fetch_assoc($result);
+			$directorID = $row['directorID'];
+			$query2 = "SELECT * FROM directedBy WHERE directorID = '$directorID' AND movieID = '$itemID'";
+			$result2 = mysqli_query($conn, $query2);
+			if (mysqli_num_rows($result2) > 0) {
+				//if director is already associated with movie, do nothing
+			}
+			else{
+				//if director is not associated with movie, add to directedby table
+				$query3 = "INSERT INTO directedBy (directorID, movieID) VALUES ('$directorID', '$itemID')";
+				mysqli_query($conn, $query3);
+			}
+		}
+		else{
+			//if director does not exist, add to database
+			$query4 = "INSERT INTO directors (directorName) VALUES ('$directorName')";
+			mysqli_query($conn, $query4);
+			//get directorID
+			$directorID = mysqli_insert_id($conn);
+			//add to directedby table
+			$query6 = "INSERT INTO directedBy (directorID, movieID) VALUES ('$directorID', '$itemID')";
+			mysqli_query($conn, $query6);
+		}
+	}
+	//delete any directors that are no longer associated with movie
+	$query5 = "SELECT * FROM directors JOIN directedBy ON directors.directorID = directedBy.directorID WHERE movieID = '$itemID'";
+	$result3 = mysqli_query($conn, $query5);
+	if (mysqli_num_rows($result3) > 0) {
+		while($row = mysqli_fetch_assoc($result3)){
+			if(!in_array($row['directorName'], $directorArray)){
+				$directorID = $row['directorID'];
+				$query7 = "DELETE FROM directedBy WHERE directorID = '$directorID' AND movieID = '$itemID'";
+				mysqli_query($conn, $query7);
+			}
+		}
+	}
+}
+
+function updateBrand($itemID, $brand, $conn){
+	//check if brand exists in database
+	$query = "SELECT * FROM brands WHERE brandName = '$brand'";
+	$result = mysqli_query($conn, $query);
+	if (mysqli_num_rows($result) > 0) {
+		//if brand exists check if brand is already associated with tech
+		$row = mysqli_fetch_assoc($result);
+		$brandID = $row['brandID'];
+		$query2 = "SELECT * FROM manufacturedBy WHERE brandID = '$brandID' AND techID = '$itemID'";
+		$result2 = mysqli_query($conn, $query2);
+		if (mysqli_num_rows($result2) > 0) {
+			//if brand is already associated with tech, do nothing
+		}
+		else{
+			//if brand is not associated with tech, add to manufacturedby table
+			$query3 = "INSERT INTO manufacturedBy (brandID, techID) VALUES ('$brandID', '$itemID')";
+			mysqli_query($conn, $query3);
+		}
+	}
+	else{
+		//if brand does not exist, add to database
+		$query4 = "INSERT INTO brands (brandName) VALUES ('$brand')";
+		mysqli_query($conn, $query4);
+		//get brandID
+		$brandID = mysqli_insert_id($conn);
+		//add to manufacturedby table
+		$query6 = "INSERT INTO manufacturedBy (brandID, techID) VALUES ('$brandID', '$itemID')";
+		mysqli_query($conn, $query6);
 	}
 }
 ?>
